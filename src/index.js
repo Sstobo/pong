4a487efbb6547fee445e239c7757834dac1d9a85
Sstobo <seanstobo@gmail.com>
2017-11-02 13:38:07 -0700

    graphics did

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import Game from './partials/Game'
 // create a game instance
 const game = new Game('game', 512, 256);
 
+
+// render repeat
+
 (function gameLoop() {
     game.render();
+    // sets loop to browser refresh rate
     requestAnimationFrame(gameLoop);
 })();
